0628e28ba2f8da36e35576a30a2a6bd906e0e8e8
0liiii <162018780+0liiii@users.noreply.github.com>
2024-05-04 13:07:51 +0200

    Create company>exceptions InvalidAgeExceptions.java

---
+++ company>exceptions InvalidAgeExceptions.java
@@ -0,0 +1,9 @@
+// company/exception/InvalidAgeExceptions.java
+
+package company.exceptions;
+
+public class InvalidAgeExceptions extends Exceptions {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
